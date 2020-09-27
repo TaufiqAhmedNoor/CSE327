f44dc8d5c47983be1345a38a74a15b28b3276fec
ShaifurRahmanShawon <42489044+ShaifurRahmanShawon@users.noreply.github.com>
2020-09-27 16:29:25 +0600

    Update Review.php

--- Review.php
+++ Review.php
@@ -20,6 +20,7 @@
     <div class="input-ground">
         <button type="submit" name="Review" class="btn">Submit</button>
     </div>
+        <!-- this section is for back to the cart page -->
     <div class="input-ground">
         <button type="submit" name="Checkout" class="btn"><a href="details.php">checkout</a></button>
     </div>
